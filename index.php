880c6553a6d8a78537c90ab63945b280f6c7f895
sXule <jsxule@gmail.com>
2014-06-04 10:57:06 -0500

    Added stylesheets

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 <html lang="en">
 <head>
 	<title>JordanCreations</title>
+	<link rel="stylesheet" type="text/css" href="reset.css"/>
+	<link rel="stylesheet" type="text/css" href="style.css"/>
 </head>
 <body>
 	<h1>This is a future release!</h1>
