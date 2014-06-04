a7774900a3c3b7f5861f2ad3a026c9230b291ec4
sXule <jsxule@gmail.com>
2014-06-04 17:37:41 -0500

    index

--- index.php
+++ index.php
@@ -6,6 +6,14 @@
 	<link rel="stylesheet" type="text/css" href="style.css"/>
 </head>
 <body>
-	<h1>This is a future release!</h1>
+
+<div id="wrapper">
+	<div id="main-div">
+		<div class="bubble">
+			<p>Hello</p>
+		</div>
+	</div>
+</div>
+
 </body>
 </html>
