5f3b044733ff57f67ee6d33584ca1cf24cd0cda6
sXule <jsxule@gmail.com>
2014-06-14 23:32:55 -0500

    Design updates

--- index.php
+++ index.php
@@ -7,13 +7,60 @@
 </head>
 <body>
 
-<div id="wrapper">
+<div id="wrapper-div">
+    <div id="head-div">
+        <img src="images/logo.gif"/>
+    </div>
+    
+    <div id="separator-div">
+        <div class="outer-center">
+            <div class="inner-center">
+                <div id="nav-div">
+                    <ul>
+                        <li class="nav-selected"><a href="#">All</a></li>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">Photo</a></li>
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="#">About</a></li>
+                    </ul>
+                </div><!-- nav-div -->
+            </div><!-- inner-center -->
+        </div><!-- outer-center -->
+        <div class="clear"></div>
+    </div><!-- separator-div -->
+    
 	<div id="main-div">
+        
 		<div class="bubble">
-			<p>Hello</p>
+			<p>Banh mi butcher master cleanse cornhole locavore jean shorts. Disrupt meh keffiyeh polaroid Kickstarter Echo Park Marfa deep v tote bag, single-origin coffee church-key kitsch gastropub. Locavore narwhal Schlitz bespoke, organic Tonx blog twee ennui. Narwhal lo-fi gentrify YOLO pour-over. Thundercats Echo Park letterpress, small batch keytar butcher salvia next level Shoreditch. Austin umami fanny pack mlkshk next level, swag YOLO church-key. Hella locavore keffiyeh crucifix American Apparel, typewriter whatever letterpress aesthetic XOXO skateboard.</p>
+
+<p>Synth street art polaroid iPhone Pinterest, ugh Cosby sweater fingerstache Pitchfork chia Tumblr next level pickled 8-bit. Gluten-free crucifix trust fund, art party umami Vice paleo sriracha. Banksy Cosby sweater small batch, YOLO shabby chic freegan mixtape fingerstache Pinterest lo-fi 3 wolf moon letterpress cliche bitters tattooed. Intelligentsia Thundercats seitan leggings quinoa you probably haven't heard of them. Single-origin coffee beard Odd Future swag jean shorts, church-key Godard. Typewriter crucifix asymmetrical Thundercats hashtag whatever, salvia tofu ethical brunch Cosby sweater organic. Gentrify Thundercats tattooed Blue Bottle, chia raw denim Echo Park pug.</p>
 		</div>
-	</div>
-</div>
+        
+        <div class="bubble">
+			<p>Banh mi butcher master cleanse cornhole locavore jean shorts. Disrupt meh keffiyeh polaroid Kickstarter Echo Park Marfa deep v tote bag, single-origin coffee church-key kitsch gastropub. Locavore narwhal Schlitz bespoke, organic Tonx blog twee ennui. Narwhal lo-fi gentrify YOLO pour-over. Thundercats Echo Park letterpress, small batch keytar butcher salvia next level Shoreditch. Austin umami fanny pack mlkshk next level, swag YOLO church-key. Hella locavore keffiyeh crucifix American Apparel, typewriter whatever letterpress aesthetic XOXO skateboard.</p>
+
+<p>Synth street art polaroid iPhone Pinterest, ugh Cosby sweater fingerstache Pitchfork chia Tumblr next level pickled 8-bit. Gluten-free crucifix trust fund, art party umami Vice paleo sriracha. Banksy Cosby sweater small batch, YOLO shabby chic freegan mixtape fingerstache Pinterest lo-fi 3 wolf moon letterpress cliche bitters tattooed. Intelligentsia Thundercats seitan leggings quinoa you probably haven't heard of them. Single-origin coffee beard Odd Future swag jean shorts, church-key Godard. Typewriter crucifix asymmetrical Thundercats hashtag whatever, salvia tofu ethical brunch Cosby sweater organic. Gentrify Thundercats tattooed Blue Bottle, chia raw denim Echo Park pug.</p>
+		</div>
+        
+        <div class="bubble">
+			<p>Banh mi butcher master cleanse cornhole locavore jean shorts. Disrupt meh keffiyeh polaroid Kickstarter Echo Park Marfa deep v tote bag, single-origin coffee church-key kitsch gastropub. Locavore narwhal Schlitz bespoke, organic Tonx blog twee ennui. Narwhal lo-fi gentrify YOLO pour-over. Thundercats Echo Park letterpress, small batch keytar butcher salvia next level Shoreditch. Austin umami fanny pack mlkshk next level, swag YOLO church-key. Hella locavore keffiyeh crucifix American Apparel, typewriter whatever letterpress aesthetic XOXO skateboard.</p>
+
+<p>Synth street art polaroid iPhone Pinterest, ugh Cosby sweater fingerstache Pitchfork chia Tumblr next level pickled 8-bit. Gluten-free crucifix trust fund, art party umami Vice paleo sriracha. Banksy Cosby sweater small batch, YOLO shabby chic freegan mixtape fingerstache Pinterest lo-fi 3 wolf moon letterpress cliche bitters tattooed. Intelligentsia Thundercats seitan leggings quinoa you probably haven't heard of them. Single-origin coffee beard Odd Future swag jean shorts, church-key Godard. Typewriter crucifix asymmetrical Thundercats hashtag whatever, salvia tofu ethical brunch Cosby sweater organic. Gentrify Thundercats tattooed Blue Bottle, chia raw denim Echo Park pug.</p>
+		</div>
+        
+        <div class="bubble">
+			<p>Banh mi butcher master cleanse cornhole locavore jean shorts. Disrupt meh keffiyeh polaroid Kickstarter Echo Park Marfa deep v tote bag, single-origin coffee church-key kitsch gastropub. Locavore narwhal Schlitz bespoke, organic Tonx blog twee ennui. Narwhal lo-fi gentrify YOLO pour-over. Thundercats Echo Park letterpress, small batch keytar butcher salvia next level Shoreditch. Austin umami fanny pack mlkshk next level, swag YOLO church-key. Hella locavore keffiyeh crucifix American Apparel, typewriter whatever letterpress aesthetic XOXO skateboard.</p>
+
+<p>Synth street art polaroid iPhone Pinterest, ugh Cosby sweater fingerstache Pitchfork chia Tumblr next level pickled 8-bit. Gluten-free crucifix trust fund, art party umami Vice paleo sriracha. Banksy Cosby sweater small batch, YOLO shabby chic freegan mixtape fingerstache Pinterest lo-fi 3 wolf moon letterpress cliche bitters tattooed. Intelligentsia Thundercats seitan leggings quinoa you probably haven't heard of them. Single-origin coffee beard Odd Future swag jean shorts, church-key Godard. Typewriter crucifix asymmetrical Thundercats hashtag whatever, salvia tofu ethical brunch Cosby sweater organic. Gentrify Thundercats tattooed Blue Bottle, chia raw denim Echo Park pug.</p>
+		</div>
+        
+
+	</div><!-- main-div -->
+    <div id="footer-div">
+        
+    </div><!-- footer-div -->
+</div><!-- wrapper-div -->
 
 </body>
 </html>
