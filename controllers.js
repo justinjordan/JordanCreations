1527a8a43f5f8376f45c27ac9dadb9df84bd36f3
Justin Jordan <jsxule@gmail.com>
2014-07-13 15:42:44 -0500

    minor style changes

--- controllers.js
+++ controllers.js
@@ -29,10 +29,29 @@
     });
     
     /*  == HOME Controller ==  */
-    jcControllers.controller('homeCtrl', function($scope){
+    jcControllers.controller('homeCtrl', function($scope, $http){
         
+        $http.get('data/news.json')
+            .success(function(data, status, headers, config){
+                $scope.bubbles = data;    
+            })
+            .error(function(data, status, headers, config){
+                alert('error');
+                /**************************
+                ** TODO:  error handling **
+                **************************/
+            });
+                
         /*  == Initial amount of words to display for each bubble ==  */
-        $scope.word_limit = 100;
+        $scope.wordLimit = 100;
+        
+        /*  == countWords = self explanatory ==  */
+        $scope.countWords = function(input){
+            var words = input.split(' ');
+            
+            return words.length;
+        }
+        /*  == END. countWords ==  */
         
         /*  == limitWords = Parse words from input ==  */
         $scope.limitWords = function(input, limit){
@@ -65,44 +84,6 @@
             
         };
         /*  == END. limitWords ==  */
-        
-        $scope.bubbles = [
-            {
-                showAll: false,
-                imageUrl: 'images/thumbnail.jpg',
-                title: 'Post 1',
-                author: 'Justin Jordan',
-                when: '3 months ago',
-                message: 
-                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-            },
-            {
-                showAll: false,
-                imageUrl: 'images/thumbnail.jpg',
-                title: 'Post 2',
-                author: 'Justin Jordan',
-                when: '3 months ago',
-                message: 
-                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-            },
-            {
-                showAll: false,
-                imageUrl: 'images/thumbnail.jpg',
-                title: 'Post 3',
-                author: 'Justin Jordan',
-                when: '3 months ago',
-                message: 
-                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-            }
-        ];
-        
-        
-        /*  == Generate limited_msg ==  */
-        for ( i = 0; i < $scope.bubbles.length; i++ )
-        {
-            $scope.bubbles[i].limited_msg = $scope.limitWords($scope.bubbles[i].message, $scope.word_limit);
-        }
-        /*  == END.  Generate limited_msg ==  */
     });
 
     /*  == BLOG Controller ==  */
