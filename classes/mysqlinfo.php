e92ac35d528456a6c78161d456fbbd9692f3da3e
Justin Jordan <jsxule@gmail.com>
2016-07-27 21:11:09 -0500

    add mysqlinfo template

--- classes/mysqlinfo.php
+++ classes/mysqlinfo.php
@@ -2,5 +2,5 @@
 
 define( 'MYSQL_HOST', 'localhost' );
 define( 'MYSQL_USER', 'root' );
-define( 'MYSQL_PASS', 'mypass123' );
+define( 'MYSQL_PASS', 'some6super6long6fricken6password' );
 define( 'MYSQL_DB', 'jordancreations' );
